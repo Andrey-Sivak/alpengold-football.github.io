4b229f43bff6a04edb1f3ae27e2bc92c042e4687
a.sivak <riziy_bes@mail.ru>
2021-08-31 03:12:24 +0300

    init

---
+++ app/js/main.js
@@ -0,0 +1,104 @@
+'use strict';
+
+const mobileWidth = 767;
+let isMobile = checkWidth();
+
+window.addEventListener('resize', () => {
+    isMobile = checkWidth();
+});
+
+setTimeout(() => {
+    if (!document.querySelector('.loader')) {
+        return;
+    }
+
+    const loader = document.querySelector('.loader');
+    if (loader.classList.contains('active')) {
+        loader.classList.remove('active');
+
+        setTimeout(() => {
+            loader.parentElement.removeChild(loader);
+        }, 300)
+    }
+}, 2500);
+
+window.addEventListener('load', function () {
+
+    (function loader() {
+        if (!document.querySelector('.loader')) {
+            return;
+        }
+
+        const loader = document.querySelector('.loader');
+
+        if (loader.classList.contains('active')) {
+            loader.classList.remove('active');
+        }
+
+        setTimeout(() => {
+            loader.parentElement.removeChild(loader);
+        }, 1500);
+
+    })();
+
+    (function timer() {
+        if (!document.querySelector('.counter__wrap')) {
+            return;
+        }
+
+        const eventDate = new Date(2021, 9, 15);
+
+        const numbers = {
+            mins: document.querySelector('.counter__item.mins'),
+            hours: document.querySelector('.counter__item.hours'),
+            days: document.querySelector('.counter__item.days'),
+        };
+
+        setDate();
+
+        setInterval(() => {
+            setDate();
+        }, 60000);
+
+        for (let num in numbers) {
+            if (numbers[num].classList.contains('hide')) {
+                numbers[num].classList.remove('hide');
+            }
+        }
+
+        function diffDates(day_one, day_two) {
+            return (day_one - day_two) / (60 * 60 * 24 * 1000);
+        }
+
+        function showData(elem, number) {
+            const num = number.toString();
+            if (num.length < 2) {
+                elem.children[0].innerHTML = '0';
+                elem.children[1].innerHTML = num;
+                return;
+            }
+
+            elem.children[0].innerHTML = num[0];
+            elem.children[1].innerHTML = num[1];
+        }
+
+        function setDate() {
+            const date = new Date();
+            const dateNow = diffDates(eventDate,
+                new Date(date.getFullYear(),
+                    date.getMonth() + 1,
+                    date.getDate()));
+            const hoursNow = 23 - date.getHours();
+            const minutesNow = 60 - date.getMinutes();
+
+            showData(numbers.days, dateNow);
+            showData(numbers.hours, hoursNow);
+            showData(numbers.mins, minutesNow);
+        }
+    })();
+
+});
+
+function checkWidth() {
+    return mobileWidth > document.documentElement.clientWidth;
+}
